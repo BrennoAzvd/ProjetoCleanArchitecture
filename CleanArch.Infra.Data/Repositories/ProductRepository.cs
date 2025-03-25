79c80b869d69c9d1c7ddb2cb913fcf824b228e65
Brenno Azevedo <brennoazevedo19@gmail.com>
2025-03-24 23:07:28 -0300

    mapeamento

---
+++ CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            return await _context.Products.FindAsync(id);
+
+        }
+
+        void IProductRepository.Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Product> IProductRepository.GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProductRepository.Remove(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProductRepository.Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
